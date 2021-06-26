02e41aab839a32164129208f4f9be817cd525608
Richard Ng <richardcrng@gmail.com>
2021-06-26 15:46:41 +0100

    Document swapIndices function

--- src/swap-indices.ts
+++ src/swap-indices.ts
@@ -1,3 +1,10 @@
+/**
+ * Create a new string by swapping characters at given indices
+ * @param str string to rearrange letters of
+ * @param idxOne first index for the swap
+ * @param idxTwo second index for the swap
+ * @returns a new string where characters at `idxOne` and `idxTwo` have been swapped
+ */
 const swapIndices = (str: string, idxOne: number, idxTwo: number): string => {
   let resultStr = "";
   for (let index = 0; index < str.length; index++) {
