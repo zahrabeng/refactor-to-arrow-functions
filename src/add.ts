7d1b289544140476ea1a50f90a97ccf8e3894a91
Zahra <bghzahra@outlook.com>
2022-03-07 12:07:23 +0000

    added oneline implicit return arrow functions

--- src/add.ts
+++ src/add.ts
@@ -5,9 +5,7 @@
  * @param secondNum second number to add
  * @returns total of the two numbers
  */
-const add = (firstNum: number, secondNum: number): number => {
-  const total = firstNum + secondNum;
-  return total;
-};
+const add = (firstNum: number, secondNum: number): number => firstNum + secondNum;
+
 
 export default add;
--- src/divide.ts
+++ src/divide.ts
@@ -5,9 +5,6 @@
  * @param secondNum second number (denominator)
  * @returns result of dividing `firstNum` by `secondNum`
  */
-const divide = (firstNum: number, secondNum: number): number => {
-  const result = firstNum / secondNum;
-  return result;
-};
+const divide = (firstNum: number, secondNum: number): number => firstNum/secondNum;
 
 export default divide;
